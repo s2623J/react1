bdb9e8c285168d4292ee4df2729a0be6ff379d70
David Michael <devdavem@yahoo.com>
2022-05-11 16:21:47 -0700

    Added styling from 'emotion' framework

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-// import logo from './logo.svg';
+import styled from '@emotion/styled';
 import './App.css';
+
+const Header = styled.h1({
+    padding: 16,
+    color: 'hotpink'
+})
+
+const Photo = styled.img({
+    display: 'block',
+    width: '100%',
+    objectFit: 'cover'
+})
+
+const TileView = styled.div( props => ({ // Allows for SASS-like variable passing
+    display: 'grid',
+    padding: props.spacing,
+    gridGap:    props.spacing,
+    gridTemplateColumns: `repeat(auto-fill, minmax(${props.minCellWidth}px, 1fr))`
+}))
+
+TileView.defaultProps = {
+    spacing: 16,
+    minCellWidth: 240
+}
 
 class App extends React.Component {
     state = {
@@ -22,16 +44,20 @@ class App extends React.Component {
         console.log('State: ', this.state);
         return (
             <div>
-                <h1>Picsum</h1>
-                {
-                    this.state.images.map(image => 
-                        <img 
-                            key={image.id} 
-                            src={`https://picsum.photos/200/300?image=${image.id}`} 
-                            alt="">
-                        </img>
-                    )
-                }
+                <Header>
+                    <h1>Picsum</h1>
+                </Header>
+                <TileView>
+                    {
+                        this.state.images.map(image => 
+                            <Photo 
+                                key={image.id} 
+                                src={`https://picsum.photos/1000/300?image=${image.id}`} 
+                                alt="">
+                            </Photo>
+                        )
+                    }
+                </TileView>
             </div>
         )
     }
